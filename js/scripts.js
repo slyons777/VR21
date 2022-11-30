446476a328c1df2ef326cf3b767d02380d664d37
Sarina Lyons <sarinalyons@gmail.com>
2022-11-30 12:22:47 -0500

    javascript basics practice

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+console.log("Is this working");
+
+const firstName = "Sarina";
+console.log(`My name is ${firstName}`);
+
+const age =33;
+console.log(`I am ${age} years old.`);
+
+const ages = [32,22,45,75]
+console.log(`There are several attendees whose ages are ${ages}`);
+
+const addAges = ages.push(23,24)
+console.log(`Although "ages" is a const, let's see if it changes. Ages: ${ages}`);
+
+const isMember = false;
+console.log(`Is this person a member: ${isMember}`);
+
+const number = "45"
+console.log(`Number is a typeof ${typeof number}`);
+
+let lastName = "Lyons"
+console.log(`My last name is ${lastName}`);
+
+lastName = "Gracia";
+console.log(`Can the last name be changed? Let's see by printing again: ${lastName}`)
+
+const num = 75
+console.log(`This is a primitive number and cannot be changed as we'll see in the next line. First print: ${num}`);
+
+// num = 85;
+// console.log(`Second "num" print: ${num}`)
+
+let student;
+console.log(`What is the value of student if it is not declared, but not defined -> ${student}`);
+
+const language1 = "Spanish"
+console.log(`I speak ${language1}, but not too well.`);
+
+const language2 = "Korean"
+console.log(`I speak ${language1}, but not too well. My brother however, is fluent in ${language2}`);
+
+const favFood = "Mexican"
+const favFood2 = "Sushi"
+console.log("This statement is printed using basic '+' sign concatenation: I like " + favFood + " food and I like " + favFood2 + ".")
+
+const spaceship = {
+    color: null,
+    size: null
+};
+console.log(`Spaceship is an const, but it is an object so I can add to it later >> First print: ${spaceship}`)
+
+
+spaceship.color = "Gray"
+spaceship.size = "Really big"
+console.log(`Now my spaceship object has some values, it is a ${spaceship.size}, ${spaceship.color} spaceship.`)
+console.log(spaceship)
+
+
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+// console.log("Is this working");
+
+

9bded3711600496c58f71bb82108d0ce624a0d2e
Sarina Lyons <sarinalyons@gmail.com>
2022-12-01 17:40:29 -0500

    arrays practice

---
+++ ArrayExercises/arrays.js
@@ -0,0 +1,38 @@
+//arrays practice
+//Write the command to remove “Greg” from the array.
+const people = ["Greg", "Mary", "Devon", "James"]
+// people.shift([0])
+// console.log(people)
+
+//Write the command to remove “James” from the array.
+// people.pop([3])
+// console.log(people)
+
+//Write the command to add “Matt” to the front of the array.
+// people.unshift("Matt")
+// console.log(people)
+
+//Write the command to add your name to the end of the array.
+// people.push("Sarina")
+// console.log(people)
+
+//Write the command to make a copy of the array using slice. The copy should NOT include “Mary” or “Matt”.
+// people.slice([2])
+
+//Write the command that gives the indexOf where “Mary” is located.
+// console.log(people.indexOf("Mary"))
+
+//Write the command that gives the indexOf where “Foo” is located (this should return -1).
+// console.log(people.indexOf("Foo"))
+
+
+//Redefine the people variable with the value you started with. Using the splice command, remove “Devon” from the array and add “Elizabeth” and “Artie”. Your array should look like this when you are done [“Greg”, “Mary”, “Elizabeth”, “Artie”, “James”].
+people.splice(2,1, "Elizabeth", "Artie")
+console.log(people)
+
+
+//Create a new variable called withBob and set it equal to the people array concatenated with the string of “Bob”.
+// const withBob = people + " Bob"
+// console.log(withBob)
+
+

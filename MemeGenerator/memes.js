ea22b3b7143d1e533019772ff7930419146f9146
Sarina Lyons <sarinalyons@gmail.com>
2022-12-06 21:41:05 -0500

    MemeGenerator Updates

--- MemeGenerator/memes.js
+++ MemeGenerator/memes.js
@@ -1,6 +1,10 @@
+document.querySelector("#submit").addEventListener("click", (e) => {})
+
+
+
 document.querySelector("#submit").addEventListener("click", (e) => {
    e.preventDefault();
-   const image = document.querySelector('#image').value;
+   const image = document.querySelector('#imageURL').value;
    const topQuote = document.querySelector('#top-quote').value;
    const bottomQuote = document.querySelector('#bottom-quote').value;
 
@@ -8,12 +12,12 @@ document.querySelector("#submit").addEventListener("click", (e) => {
    const contentCard = `
    <div class="col-md-3">
           <div class="card">
-          <div class="card-body">
+            <div class="card-body">
                 <h3 class="top-quote">${topQuote}</h3>
             </div>
             <img
               src=${image}
-              class="card-img-top"
+              class="card-img"
               alt="..."/>
             <div>
                <h3 class="bottom-quote">${bottomQuote}</h3>

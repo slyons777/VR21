2c2f88649c183e2253db443ff0f2bde82e8e9e3f
Sarina Lyons <sarinalyons@gmail.com>
2022-12-13 23:57:55 -0500

    quick variable fix

--- js/Unsplash/api.js
+++ js/Unsplash/api.js
@@ -1,3 +1,5 @@
+//Demonstration of returning responses from an API
+
 document.querySelector("#searchButton").addEventListener("click", (e) => {
   //set variables to hold key and search value
   const client_id = "rLq9bvi_8PHd4HDf1G24bOnATPnByqoMjRtvGbFxMf0";
@@ -9,14 +11,14 @@ document.querySelector("#searchButton").addEventListener("click", (e) => {
   fetch(resource)
     .then((res) => res.json())
     .then((data) => {
-      const container2 = document.querySelector(".container2");
+      const container = document.querySelector(".container");
       //loop over responses and append them the container set within html
       for (let i = 0; i < data.results.length - 1; i++) {
         const img = document.createElement("img");
         const url = data.results[i].urls.thumb;
         img.setAttribute("src", url);
 
-        container2.append(img);
+        container.append(img);
       }
     });
   //clear the search bar after submit is hit

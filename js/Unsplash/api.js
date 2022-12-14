abcf2f7f145ff28fa85a29aa3d71aa0c9b665b99
Sarina Lyons <sarinalyons@gmail.com>
2022-12-13 23:55:20 -0500

    Unsplash API Example

---
+++ js/Unsplash/api.js
@@ -0,0 +1,24 @@
+document.querySelector("#searchButton").addEventListener("click", (e) => {
+  //set variables to hold key and search value
+  const client_id = "rLq9bvi_8PHd4HDf1G24bOnATPnByqoMjRtvGbFxMf0";
+  const searchStr = document.querySelector("#userInput").value;
+  const resource = `https://api.unsplash.com/search/photos?query=${searchStr}&per_page=20&client_id=${client_id}`;
+
+  //fetch from api
+  e.preventDefault();
+  fetch(resource)
+    .then((res) => res.json())
+    .then((data) => {
+      const container2 = document.querySelector(".container2");
+      //loop over responses and append them the container set within html
+      for (let i = 0; i < data.results.length - 1; i++) {
+        const img = document.createElement("img");
+        const url = data.results[i].urls.thumb;
+        img.setAttribute("src", url);
+
+        container2.append(img);
+      }
+    });
+  //clear the search bar after submit is hit
+  document.querySelector(".d-flex").reset();
+});
